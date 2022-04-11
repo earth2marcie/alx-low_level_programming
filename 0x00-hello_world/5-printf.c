f61efd8176b2fbf930754fb700e790f91df3b6c5
marcelino <mohamedmarcelino722@gmail.com>
2022-04-11 06:14:20 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry point
+ * My hello.print task
+ * Return: return 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammer , but the outcome is a piece of art ,\n");
+return (0);
+}1
