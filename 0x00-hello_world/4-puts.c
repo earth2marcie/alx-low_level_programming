85d32a229639e34bdae01e4b4c4868ed202b95e8
marcelino <mohamedmarcelino722@gmail.com>
2022-04-11 06:05:25 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
- * Return: 0 after printing the function 
+ *  Return: Always 0 (Succesess)
  */
 int main(void)
-{	
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}			
+}
