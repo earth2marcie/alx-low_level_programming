242ab99e488cd0ee75bed436ddff255b5d1cf7e8
earth2marcie <101350053+earth2marcie@users.noreply.github.com>
2022-04-22 15:29:13 +0200

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,26 +1,25 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
+*_strcat - function that concatenates teo strings
+*     two strings.
+*
+@dest: pointer to destination input
+@src: pointer to source input
+*
+Return: pointer to resulting string
+@dest
+*/
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
-}
+char *_strcat(char *dest, char *src)
+{
+int c, c2
+c = 0
+/*find the size of dest array*/
+while (dest[c])
+c++;
+/*iterate through each src array value without the null byte*/
+for (c2 = 0, src[c2]; c2++)
+/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+dest[c++] = src[c2];
+return (dest);
+}    
+    
