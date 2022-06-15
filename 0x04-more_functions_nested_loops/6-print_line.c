8f3374e02a72d67e1828b6d3739a18b71fd351d3
earth2marcie <101350053+earth2marcie@users.noreply.github.com>
2022-06-15 17:24:47 +0200

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_line - prints a line
@@ -9,12 +9,12 @@
 
 void print_line(int n)
 {
-	      int a = 0;
+	int a = 0;
 
-	      while (a < n && n > 0)
-	      {
-		            _putchar('_');
-		            a++;
-	      }
-	      _putchar('\n');
+	while (a < n && n > 0)
+	{
+	        _putchar('_');
+		a++;
+	 }
+	 _putchar('\n');
 }
